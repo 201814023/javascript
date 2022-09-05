247fb147b2c55a669f763c32d47f231a98eb10c6
201814023 <112357154+201814023@users.noreply.github.com>
2022-09-05 11:34:24 +0900

    201814023

---
+++ plus.js
@@ -0,0 +1,15 @@
+function plus(num1,num2) {
+    let result;
+    result=num1+num2;
+    return result
+}
+function minus(num1,num2) {
+    let result;
+    result=num1-num2;
+    return result
+} /// 지역변수
+function division(num1,num2) {
+    let result;
+    result=num1/num2;
+    return result
+}
